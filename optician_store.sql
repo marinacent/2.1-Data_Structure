5dad92c1a309127a44d41f99b0e6837275778b8a
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-29 22:13:45 +0200

    feat: use foreign key in recommended by

--- optician_store.sql
+++ optician_store.sql
@@ -8,7 +8,8 @@ CREATE TABLE `customers` (
   `phone_number` integer,
   `email` varchar(50),
   `registered` date DEFAULT NOW(),
-  `recommended_by` integer #foreign key?
+  `recommended_by` integer,
+  FOREIGN KEY (`recommended_by`) REFERENCES `customers` (`id`)
 );
 
 CREATE TABLE `providers`(
@@ -22,22 +23,20 @@ CREATE TABLE `providers`(
 
 CREATE TABLE `glasses` (
   `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `brand` integer,
+  `brand` integer, # should this be a varchar 'brand'??
   `prescription_right` varchar(50),
   `prescription_left` varchar(50),
   `frame_type` enum('rimless', 'plastic', 'metal'),
   `frame_color` varchar(25), #what if the frame_type is rimless?
   `color_right` varchar(25),
   `color_left` varchar(25),
-  `price` float
+  `price` float,
+  FOREIGN KEY (`brand`) REFERENCES `brands` (`id`)
   );
   
 CREATE TABLE `brands` (
   `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` varchar(50),
-  `provider` integer
+  `provider` integer,
+  FOREIGN KEY (`provider`) REFERENCES `providers` (`id`)
   );
-
-ALTER TABLE `brands` ADD FOREIGN KEY (`provider`) REFERENCES `providers` (`id`);
-
-ALTER TABLE `glasses` ADD FOREIGN KEY (`brand`) REFERENCES `brands` (`id`);
