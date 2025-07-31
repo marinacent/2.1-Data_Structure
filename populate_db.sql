5e41b558ecf0ed2106f5f5e213cf93e07dd81b6f
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-31 12:29:42 +0200

    feat: add brands to db

--- populate_db.sql
+++ populate_db.sql
@@ -4,3 +4,7 @@ INSERT INTO providers VALUES (NULL, 'Ulleres Joan', 1, '973456541', '973456542',
 INSERT INTO addresses VALUES (NULL, 'Accessoris Vives', 'Garrotxa', '22', 'entresol', '2', 'Blanes', '17300', 'Spain');
 INSERT INTO providers VALUES (NULL, 'Accessoris Vives', 2, '976567897', '976567898', 'B67891234');
 
+INSERT INTO brands VALUES (NULL, 'Furla', 1);
+INSERT INTO brands VALUES (NULL, 'Ray-Ban', 1);
+INSERT INTO brands VALUES (NULL, 'Oakley', 2);
+
