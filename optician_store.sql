80e7aa8e9122834942664246fb29e3704f31840b
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-31 11:07:48 +0200

    style: write variable types in all caps

--- optician_store.sql
+++ optician_store.sql
@@ -1,73 +1,72 @@
 CREATE DATABASE IF NOT EXISTS optician_store;
 USE optician_store;
 
-# write variable types in all caps???
-
 CREATE TABLE `addresses` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `street` varchar(50),
-  `number` integer,
-  `floor` integer,
-  `door` varchar(1),
-  `city` varchar(50),
-  `postal_code` varchar(25),
-  `country` varchar(50)
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `street` VARCHAR(50),
+  `number` INT,
+  `floor` INT,
+  `door` CHAR,
+  `city` VARCHAR(50),
+  `postal_code` VARCHAR(25),
+  `country` VARCHAR(50)
   );
   
   CREATE TABLE `providers`(
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(50),
-  `address` integer,
-  `phone_number` varchar(25),
-  `fax_number` varchar(25),
-  `nif` varchar(9),
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  `address` INT,
+  `phone_number` VARCHAR(25),
+  `fax_number` VARCHAR(25),
+  `nif` CHAR(9),
   FOREIGN KEY (`address`) REFERENCES `addresses` (`id`)
 );
 
 CREATE TABLE `brands` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(50),
-  `provider` integer,
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  `provider` INT,
   FOREIGN KEY (`provider`) REFERENCES `providers` (`id`)
   );
 
 CREATE TABLE `customers` (
   `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `address` integer,
-  `phone_number` varchar(25),
-  `email` varchar(50),
-  `registered` timestamp DEFAULT CURRENT_TIMESTAMP(),
-  `recommended_by` integer,
+  `first_name` VARCHAR(50),
+  `last_name` VARCHAR(50),
+  `address` INT,
+  `phone_number` VARCHAR(25),
+  `email` VARCHAR(50),
+  `registered` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+  `recommended_by` INT,
   FOREIGN KEY (`address`) REFERENCES `addresses` (`id`),
   FOREIGN KEY (`recommended_by`) REFERENCES `customers` (`id`)
 );
 
  CREATE TABLE `employees` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50),
-  `last_name` varchar(50)
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(50),
+  `last_name` VARCHAR(50)
   );
 
 CREATE TABLE `glasses` (
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `brand` integer, 
-  `prescription_right` varchar(50),
-  `prescription_left` varchar(50),
-  `frame_type` enum('rimless', 'plastic', 'metal'),
-  `frame_color` varchar(25),
-  `color_right` varchar(25),
-  `color_left` varchar(25),
-  `price` float,
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `brand` INT, 
+  `prescription_right` VARCHAR(50),
+  `prescription_left` VARCHAR(50),
+  `frame_type` ENUM('rimless', 'plastic', 'metal'),
+  `frame_color` VARCHAR(25),
+  `color_right` VARCHAR(25),
+  `color_left` VARCHAR(25),
+  `price` FLOAT,
   FOREIGN KEY (`brand`) REFERENCES `brands` (`id`)
   );
   
   CREATE TABLE `sales`(
-  `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `glasses` integer,
-  `customer` integer,
-  `employee` integer,
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `glasses` INT,
+  `customer` INT,
+  `employee` INT,
+  `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
   FOREIGN KEY (`glasses`) REFERENCES `glasses` (`id`),
   FOREIGN KEY (`customer`) REFERENCES `customers` (`id`),
   FOREIGN KEY (`employee`) REFERENCES `employees` (`id`)
