70a0dc5b419af20cfcab1a56dcc0a06fe2983c16
marinacent <52744370+marinacent@users.noreply.github.com>
2025-08-20 21:30:12 +0200

    feat: add db schema

--- create_db.sql
+++ create_db.sql
@@ -2,13 +2,15 @@ DROP DATABASE IF EXISTS optician_store;
 CREATE DATABASE optician_store CHARACTER SET utf8mb4;
 USE optician_store;
 
+SET FOREIGN_KEY_CHECKS = 0;
+
 CREATE TABLE `addresses` (
   `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50),
   `street` VARCHAR(50),
   `number` VARCHAR(25),
   `floor` VARCHAR(25),
-  `door` CHAR,
+  `door` CHAR(1),
   `city` VARCHAR(50),
   `postal_code` VARCHAR(25),
   `country` VARCHAR(50)
@@ -17,7 +19,7 @@ CREATE TABLE `addresses` (
   CREATE TABLE `providers`(
   `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50),
-  `id_address` INT,
+  `id_address` INT UNSIGNED,
   `phone_number` VARCHAR(25),
   `fax_number` VARCHAR(25),
   `nif` CHAR(9) UNIQUE,
@@ -27,7 +29,7 @@ CREATE TABLE `addresses` (
 CREATE TABLE `brands` (
   `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(50),
-  `id_provider` INT,
+  `id_provider` INT UNSIGNED,
   FOREIGN KEY (`id_provider`) REFERENCES `providers` (`id`)
   );
 
@@ -35,11 +37,11 @@ CREATE TABLE `customers` (
   `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(50),
   `last_name` VARCHAR(50),
-  `id_address` INT,
+  `id_address` INT UNSIGNED,
   `phone_number` VARCHAR(25),
   `email` VARCHAR(50),
   `registered` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `id_recommended_by` INT,
+  `id_recommended_by` INT UNSIGNED,
   FOREIGN KEY (`id_address`) REFERENCES `addresses` (`id`),
   FOREIGN KEY (`id_recommended_by`) REFERENCES `customers` (`id`)
 );
@@ -52,7 +54,7 @@ CREATE TABLE `customers` (
 
 CREATE TABLE `glasses` (
   `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `id_brand` INT, 
+  `id_brand` INT UNSIGNED, 
   `prescription_right` VARCHAR(50),
   `prescription_left` VARCHAR(50),
   `frame_type` ENUM('rimless', 'plastic', 'metal'),
@@ -64,9 +66,9 @@ CREATE TABLE `glasses` (
   
   CREATE TABLE `sales`(
   `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `id_glasses` INT,
-  `id_customer` INT,
-  `id_employee` INT,
+  `id_glasses` INT UNSIGNED,
+  `id_customer` INT UNSIGNED,
+  `id_employee` INT UNSIGNED,
   `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
   FOREIGN KEY (`id_glasses`) REFERENCES `glasses` (`id`),
   FOREIGN KEY (`id_customer`) REFERENCES `customers` (`id`),
