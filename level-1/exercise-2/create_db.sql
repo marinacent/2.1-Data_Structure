67aa3455cb113f4c48e644bded98ae6dd2c52bfc
marinacent <52744370+marinacent@users.noreply.github.com>
2025-08-27 12:22:56 +0200

    feat: create customers, provinces and municipalities tables

---
+++ level-1/exercise-2/create_db.sql
@@ -0,0 +1,55 @@
+DROP DATABASE IF EXISTS pizzeria;
+CREATE DATABASE pizzeria CHARACTER SET utf8mb4;
+USE pizzeria;
+
+CREATE TABLE `municipalities` (
+  `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  `province` INT UNSIGNED,
+  FOREIGN KEY (`province`) REFERENCES `provinces` (`id`)
+  );
+
+CREATE TABLE `provinces` (
+  `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50)
+  );
+
+CREATE TABLE `customers` (
+  `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50),
+  `surnames` VARCHAR(50),
+  `address` VARCHAR(100),
+  `postal_code` VARCHAR(25),
+  `municipality` INT UNSIGNED,
+  `province` INT UNSIGNED,
+  `phone_number` VARCHAR(25)
+  );
+  
+
+DELIMITER $$
+
+CREATE TRIGGER customers_before_insert
+BEFORE INSERT ON customers
+FOR EACH ROW
+BEGIN
+  IF NEW.municipality IS NOT NULL THEN
+    SET NEW.province = (
+      SELECT province FROM municipalities WHERE id = NEW.municipality
+	);
+  END IF;
+END$$
+
+CREATE TRIGGER customers_before_update
+BEFORE UPDATE ON customers
+FOR EACH ROW
+BEGIN
+  IF NEW.municipality IS NOT NULL THEN
+    SET NEW.province = (
+      SELECT province FROM municipalities WHERE id = NEW.municipality
+	);
+  END IF;
+END$$
+
+DELIMITER ;
+  
+  
