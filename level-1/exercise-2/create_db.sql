b1a0b1ae08555d17c987fd11fd2405286f638f52
marinacent <52744370+marinacent@users.noreply.github.com>
2025-08-28 12:41:47 +0200

    feat: add stores table

--- level-1/exercise-2/create_db.sql
+++ level-1/exercise-2/create_db.sql
@@ -77,7 +77,7 @@ CREATE TABLE `stores` (
   `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `address` VARCHAR(100),
   `postal_code` VARCHAR(25),
-  `municipality` INT UNSIGNED,
+  `municipality` INT UNSIGNED
   );
   
 -- add store!
