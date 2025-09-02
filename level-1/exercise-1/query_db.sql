5e1a7113342b71254c1b34c82e24b1ef673af6da
marinacent <52744370+marinacent@users.noreply.github.com>
2025-09-02 20:50:40 +0200

    refactor: change from group by to distinct

--- level-1/exercise-1/query_db.sql
+++ level-1/exercise-1/query_db.sql
@@ -8,20 +8,21 @@ WHERE c.id = 1
   AND s.date BETWEEN '2025-01-01' AND '2025-12-31'
 GROUP BY c.id, c.first_name, c.last_name;
 
-SELECT 
+SELECT DISTINCT
     e.first_name, 
     e.last_name, 
     b.name AS brand, 
     g.frame_type, 
     g.frame_color, 
-    g.lens_color
+    g.lens_color,
+    g.id AS glasses_id
 FROM sales s
 JOIN employees e ON s.id_employee = e.id
 JOIN glasses g ON s.id_glasses = g.id
 JOIN brands b ON g.id_brand = b.id
 WHERE e.id = 2
   AND YEAR(s.date) = 2025
-GROUP BY g.id;
+;
 
 SELECT DISTINCT 
     p.name AS provider_name
