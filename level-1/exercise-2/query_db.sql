7dc51964a6896065e28722de3bf1bfb09e3b7231
marinacent <52744370+marinacent@users.noreply.github.com>
2025-09-02 23:14:26 +0200

    fix: fix column name

--- level-1/exercise-2/query_db.sql
+++ level-1/exercise-2/query_db.sql
@@ -6,7 +6,7 @@ JOIN products p ON po.product_id = p.id
 JOIN orders o ON po.order_id = o.id
 JOIN stores s ON o.store_id = s.id
 WHERE p.type = 'drink'
-	AND s.municipality = 1;
+	AND s.municipality_id = 1;
     
 SELECT COUNT(*)
 FROM deliveries
