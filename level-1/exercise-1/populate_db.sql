5067d893ca16f902a5d55dd2cfa4084b84e6b4ae
marinacent <52744370+marinacent@users.noreply.github.com>
2025-09-02 20:40:43 +0200

    fix: reformat prices

--- level-1/exercise-1/populate_db.sql
+++ level-1/exercise-1/populate_db.sql
@@ -9,9 +9,9 @@ INSERT INTO brands VALUES (NULL, 'Furla', 1);
 INSERT INTO brands VALUES (NULL, 'Ray-Ban', 1);
 INSERT INTO brands VALUES (NULL, 'Oakley', 2);
 
-INSERT INTO glasses VALUES (NULL, 1, 'OD: -1.25 -0.50 x90', 'OS: -1.00 -0.25 x85', 'rimless', NULL, 'clear', 120);
+INSERT INTO glasses VALUES (NULL, 1, 'OD: -1.25 -0.50 x90', 'OS: -1.00 -0.25 x85', 'rimless', NULL, 'clear', 120.00);
 INSERT INTO glasses VALUES (NULL, 1, 'OD: +2.00 ADD +1.50' , 'OS: +1.75 ADD +1.50', 'metal', 'red', 'polarized gray', 92.50);
-INSERT INTO glasses VALUES (NULL, 2, 'OD: -2.50 -1.00 x10 ADD +2.00', 'OS: -2.25 -0.75 x170 ADD +2.00', 'plastic', 'dark green', 'transition gray', 134);
+INSERT INTO glasses VALUES (NULL, 2, 'OD: -2.50 -1.00 x10 ADD +2.00', 'OS: -2.25 -0.75 x170 ADD +2.00', 'plastic', 'dark green', 'transition gray', 134.00);
 INSERT INTO glasses VALUES (NULL, 3, 'OD: 0.00 -0.75 x180', 'OS: 0.00 -0.50 x175', 'metal', 'pink', 'clear', 100.99);
 
 INSERT INTO addresses VALUES (NULL, 'Consell de Cent', '138', '5', '2', 'Barcelona', '08013', 'Spain');
