9c231c886de766add98dd9f15d35ea619f779064
marinacent <52744370+marinacent@users.noreply.github.com>
2025-09-03 14:39:02 +0200

    feat: create videos table

--- level-2/create_db.sql
+++ level-2/create_db.sql
@@ -12,3 +12,24 @@ CREATE TABLE users (
   country VARCHAR(50),
   postal_code VARCHAR(25)
   );
+
+CREATE TABLE videos (
+  id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  user_id INT UNSIGNED NOT NULL,
+  title VARCHAR(100) NOT NULL,
+  description VARCHAR(5000),
+  size FLOAT,
+  file_name VARCHAR(255),
+  length TIME,
+  thumbnail LONGBLOB,
+  number_of_views INT UNSIGNED,
+  number_of_likes INT UNSIGNED,
+  number_of_dislikes INT UNSIGNED 
+  );
+  
+  
+  
+  
+  
+  
+  
