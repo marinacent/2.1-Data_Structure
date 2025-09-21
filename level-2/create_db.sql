d6c46c442c64a0dc59f1092293a8f8c876ba8cc2
marinacent <52744370+marinacent@users.noreply.github.com>
2025-09-21 18:52:32 +0200

    fix: make usernames and emails unique

--- level-2/create_db.sql
+++ level-2/create_db.sql
@@ -4,9 +4,9 @@ USE youtube;
 
 CREATE TABLE users (
   id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
   password CHAR(60) NOT NULL,
-  username VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL UNIQUE,
   date_of_birth DATE,
   gender ENUM('male', 'female', 'other'),
   country VARCHAR(50),
