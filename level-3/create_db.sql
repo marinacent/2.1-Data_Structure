8fc7783803b8267a6754b310056aa95c455ba23b
marinacent <52744370+marinacent@users.noreply.github.com>
2025-09-21 19:02:23 +0200

    feat: create users table

--- level-3/create_db.sql
+++ level-3/create_db.sql
@@ -2,3 +2,14 @@ DROP DATABASE IF EXISTS spotify;
 CREATE DATABASE spotify CHARACTER SET utf8mb4;
 USE spotify;
 
+CREATE TABLE users (
+  id INT UNSIGNED PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password CHAR(60) NOT NULL,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  date_of_birth DATE,
+  gender ENUM('male', 'female', 'other'),
+  country VARCHAR(50),
+  postal_code VARCHAR(25)
+  );
+  
